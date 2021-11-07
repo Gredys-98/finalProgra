d1ae246092b593c9e7eead422f03c65cb5eaa777
Gredys-98 <gredyperez9@gmail.com>
2021-11-07 16:50:53 -0600

    PAgina HTML

---
+++ src/main/java/gt/edu/umg/proyecto/dao/InterDepartamentoDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.umg.proyecto.dao;
+
+import gt.edu.umg.proyecto.dto.Departamento;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Carrillo
+ * 
+ */
+public interface InterDepartamentoDao extends CrudRepository<Departamento,Integer>{
+    
+}
